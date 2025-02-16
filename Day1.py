9a5131a470b9a7f92164eeff1ed16ed444293ea1
Bunny@4898 <147973215+BUNNyO111@users.noreply.github.com>
2025-02-16 19:55:16 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,79 @@
+# print("Hello World")
+
+# print("Praneeth \n")
+# print("Hello" + input("name") + "$")
+# input("Fuck\n")
+
+# print(len("Hello"))
+# a = "Hellow world3"
+# print(len(a))
+
+# Username = input("Username")
+# length = len(Username)
+# print(length)
+
+# print(float(1.32+2))
+
+# print(len("123"))
+
+# print(123.98 + 123)
+
+
+# a = int("5") / int(2.7)
+# print(a)
+
+# print("welcome to tip calculator")
+# x = float(input("total Bill"))
+# y = int(input("what will you slipt"))
+# s = int(input("no of splits"))
+
+# z = x/y
+# M  = x + z
+# D = M /s
+# print(round(D , 2))
+
+
+# hieght = (int(input("Heigi")))
+
+# if hieght > 120:
+#     print("OK")
+# else:
+#     print("sorry")
+
+# height = int(input("Enter your height in cm: "))
+
+# if height > 120:
+#     print("You meet the height requirement! ✅")
+# else:
+#     print("Sorry, you do not meet the height requirement. ❌")
+
+# X = print(int(Input("Number")))
+# R = X/2 = 
+# if 
+
+# weight = 85
+# height = 1.85
+
+# bmi = weight / (height ** 2)
+
+# # 🚨 Do not modify the values above
+# # Write your code below 👇
+
+# if bmi > 18.5:
+#     print("underweight")
+    
+# elif bmi >= 18.5 and bmi <= 25:
+#     print("normal weight")
+# else:
+#     print("overweight")
+
+    
+
+Number_Q = 2345
+
+
+
+for i in range(1 , 101):
+    if i*3 == 0:
+        print("Fizz")
+    else print(i)
